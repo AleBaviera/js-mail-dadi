d8db493e2318d9d50cb3b6837c820c77354e47c8
alebaviera <baviera.alessandra@gmailcom>
2019-07-02 13:51:35 +0200

    prova js

--- mail/script.js
+++ mail/script.js
@@ -1,12 +1,22 @@
 // 1 -Chiedi all’utente la sua email,  controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato;
+// dichiaraz var
+var mail;
 
 // creazione array
-
+var mailinglist = ['pino@gmail.com', 'giuse@gmail.com', 'maria@gmail.com', 'fede@gmail.com', 'giulio@gmail.com', 'ale@gmail.com']
+console.log(mailinglist);
 
 // inserimento mail utente
-
-
+mail = prompt('inserisci la tua email');
+console.log(mail);
 // confronto con array
-
+for (var i = 0, i <= mailinglist.length, i++){
+  if (mail === mailinglist[i]){
+    document.getElementById('mio-id').innerHTML = 'ok, prosegui!';
+  }
+  else{
+    document.getElementById('mio-id').innerHTML = 'non puoi accedere!';
+  }
+}
 
 // esito
