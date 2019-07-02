ed55f7a270eee4ae4ed17628193600f0b46a0591
alebaviera <baviera.alessandra@gmailcom>
2019-07-02 13:26:55 +0200

    mail scheletro js

---
+++ mail/script.js
@@ -0,0 +1,12 @@
+// 1 -Chiedi all’utente la sua email,  controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato;
+
+// creazione array
+
+
+// inserimento mail utente
+
+
+// confronto con array
+
+
+// esito
